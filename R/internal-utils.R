062eae6e2fa8eb97a0e9f6bfa46a46f12d575167
Olivia Beck <olb5053@psu.edu>
2024-11-04 10:20:48 -0500

    adding internal functions documentation

--- R/internal-utils.R
+++ R/internal-utils.R
@@ -15,16 +15,30 @@
 #' - Conversions from igraph objects to edge lists are handled with `igraph::as_edgelist`.
 #' - Conversions from igraph objects to adjacency matrices are handled with `igraph::as_adjacency_matrix`.
 #'
+#' Networks with \eqn{n} nodes can be represented as,
+#' \itemize{
+#'    \item{"igraph": }{an igraph object}
+#'    \item{"matrix": }{an \eqn{n}-by-\eqn{n} adjacency matrix, class `"matrix"`}
+#'    \item{"dgCMatrix": }{an \eqn{n}-by-\eqn{n} adjacency matrix as a sparse matrix, class `{dgCMatrix}`, see \link[Matrix]{Matrix} package for details}
+#'    \item{"edgelist": }{an \eqn{\binom{n}{2}}-by-2 matrix where each row is the pair of nodes an edge is attached to}
+#' }
 #' To use `"dgCMatrix"` for `input.type` or `output.type`, the package `Matrix` must be installed.
+#' @seealso [detect_type()]
 #'
 #' @examples
 #' # Convert list of adjacency matrices to igraph objects
-#' adj_matrices <- replicate(3, matrix(c(0, 1, 1, 0), nrow = 2), simplify = FALSE)
-#' make_network_type(adj_matrices, input.type = "matrix", output.type = "igraph")
+#' one_matrix <- matrix(c(0, 1, 1, 1, 0, 0, 1, 0, 0), nrow = 3, byrow = TRUE)
+#' matrix_list <- replicate(3, one_matrix, simplify = FALSE)
+#' igraph_list <- make_network_type(matrix_list, input.type = "matrix", output.type = "igraph")
 #'
-#' # Convert list of igraph objects to sparse adjacency matrices
-#' igraph_list <- lapply(adj_matrices, igraph::graph_from_adjacency_matrix)
-#' make_network_type(igraph_list, input.type = "igraph", output.type = "dgCMatrix")
+#' # Same thing now using detect type
+#' type <- detect_type(matrix_list)
+#' type
+#' igraph_list <- make_network_type(matrix_list, input.type = type, output.type = "igraph")
+#'
+#' # Convert list of igraph objects to sparse matrix
+#' sparsematrix_list <- make_network_type(igraph_list, input.type = "igraph", output.type = "dgCMatrix")
+#' detect_type(sparsematrix_list)
 #'
 #' @importFrom igraph graph_from_adjacency_matrix as_adjacency_matrix as_edgelist
 #' @importFrom Matrix Matrix
@@ -77,24 +91,30 @@ make_network_type <- function(obj, input.type, output.type){
 
 #' Detect the Type of Objects in a List
 #'
-#' Attempts to detect the type of objects in a list based on their class and dimensions, as used in the JaB package. This function checks if all elements in the list are of the same class and determines their type, such as `"igraph"`, `"dgCMatrix"`, `"edgelist"`, or `"matrix"`.
+#' Attempts to detect the type of network representation in a list.  Networks can be represented as `"igraph"`, `"dgCMatrix"`, `"edgelist"`, or `"matrix"`.
 #'
-#' @param obj A list of objects to be analyzed. All objects in the list must belong to the same class.
+#' @param obj A list of networks. All networks in the list must have the same representation. e.g. all networks in the list must be an edge list.
 #'
-#' @return A character string indicating the type of objects in the list. Possible return values are:
+#' @return A character string indicating the representation of the networks in the list. Possible return values are:
 #' \itemize{
-#'   \item `"igraph"`: If the objects are igraph objects.
-#'   \item `"dgCMatrix"`: If the objects are sparse matrices of class `dgCMatrix`.
-#'   \item `"edgelist"`: If the objects are matrices with two columns, representing an edge list.
-#'   \item `"matrix"`: If the objects are square matrices, typically representing adjacency matrices.
+#'   \item `"igraph"`: If the networks are igraph objects.
+#'   \item `"dgCMatrix"`: If the networks are sparse matrices of class `dgCMatrix`.
+#'   \item `"edgelist"`: If the networks are matrices with two columns, representing an edge list.
+#'   \item `"matrix"`: If the networks are square matrices, typically representing adjacency matrices.
 #' }
 #'
 #' @details
-#' The function first verifies that all objects in the list are of the same class. It then checks if the class matches known types (`"igraph"` or `"dgCMatrix"`). If the class is not one of these, it further examines the dimensions of each object to determine if they represent an edge list (two-column matrix) or an adjacency matrix (square matrix).
+#' The function detects if the input list of networks are represented as
+#' `"igraph"`, `"dgCMatrix"`, `"edgelist"` or `"matrix"`. This function is primarily
+#' used internally for verifying the networks are in the correct format for the
+#' intended operation being used.
+#'
+#' 2-by-2 matrices default to `"matrix"`.
+#'
+#' @inherit make_network_type examples
+#' @seealso [make_network_type()]
+#' @export
 detect_type <- function(obj){
-
-  #input obj = list of objects
-  #attempts to detect type of object as related to JaB package
 
   classes <- unique(unlist(lapply(obj, class)))
   if("igraph" %in% classes){
@@ -119,13 +139,17 @@ detect_type <- function(obj){
 #' Extracts the names of nodes (vertices) from a network object.
 #' This is for internal use for network types in the JaB package
 #'
-#' @param network A network object with \eqn{n}, which may be of types `matrix`, `dgCMatrix` `edgelist`, or `igraph`).
+#' @param network A network object with \eqn{n} nodes. Network can be represented as a `matrix`, `dgCMatrix` `edgelist`, or `igraph`). See \code{\link{make_network_type}} for details.
 #'
-#' @return A vector of node names. If node names are not assigned, it returns a sequence of integers from 1 to the number of nodes.
+#' @return A vector of node names. If node names are not assigned, it returns a sequence of integers from 1 to \eqn{n}.
 #' @seealso \code{\link{detect_type}}, \code{\link{make_network_type}}
 #' @importFrom igraph V gorder
+#' @examples
+#' data(paul.revere)
+#' get_nodes(paul.revere)
+#' @export
 get_nodes <- function(network){
-  class <- JaB:::detect_type(list(network))
+  class <- detect_type(list(network))
   network <- make_network_type(list(network), class, "igraph")[[1]]
   names <- igraph::V(network)$name
   if(is.null(names)){names <- 1:igraph::gorder(network)}
